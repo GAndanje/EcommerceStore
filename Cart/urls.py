73ae375f39e4fa9131d87764be7692c4e41659b0
Gilbert <gilbertandanje@gmail.com>
2022-10-14 06:34:40 +0300

    #5cc4aa1

--- Cart/urls.py
+++ Cart/urls.py
@@ -3,8 +3,4 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.cart,name="cart"),
-    path('add_cart/<int:product_id>/',views.add_cart,name="add_cart"),
-    path('decrement_cart/<int:product_id>/',views.decrement_cart,name="decrement_cart"),
-    path('remove_from_cart/<int:product_id>/',views.remove_from_cart,name="remove_from_cart"),
-
 ]
