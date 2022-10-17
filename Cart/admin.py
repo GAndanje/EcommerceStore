f93b903e3f258c4fd369525bbab3ae8d26a4965e
Gilbert <gilbertandanje@gmail.com>
2022-10-17 23:23:40 +0300

    created CartAdmin to render Cart model in the Admin panel

--- Cart/admin.py
+++ Cart/admin.py
@@ -4,3 +4,7 @@ from .models import Cart,CartItem
 class CartItemAdmin(admin.ModelAdmin):
     list_display=('product','cart','quantity','is_active')
 admin.site.register(CartItem,CartItemAdmin)
+
+class CartAdmin(admin.ModelAdmin):
+    list_display=('cart_id','added_date')
+admin.site.register(Cart,CartAdmin)
