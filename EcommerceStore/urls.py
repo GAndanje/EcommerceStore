af2ec1a42e4628f0dfeea21911982e628922e8bc
Gilbert <gilbertandanje@gmail.com>
2022-10-17 23:27:22 +0300

    changed relative url to explicitly defined url

--- EcommerceStore/urls.py
+++ EcommerceStore/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 from EcommerceStore.settings import MEDIA_URL
-from . import views
+from EcommerceStore import views
 from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
