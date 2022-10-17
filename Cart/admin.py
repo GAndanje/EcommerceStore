5cadf7ea8e4914d0b4d1fcbea349c82f1bb757c6
Gilbert <gilbertandanje@gmail.com>
2022-10-17 23:22:12 +0300

    created a CartItemAdmin to render CartItems model in the admin panel

--- Cart/admin.py
+++ Cart/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Cart,CartItem
 # Register your models here.
-admin.site.register(CartItem)
-admin.site.register(Cart)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display=('product','cart','quantity','is_active')
+admin.site.register(CartItem,CartItemAdmin)
