a7dcb9ecbdd76a536574c9192de7874b8eb81f30
Gilbert <gilbertandanje@gmail.com>
2022-10-17 23:49:41 +0300

    Created  ProductVariation admin class to handle the ProductVariations model class

--- Store/admin.py
+++ Store/admin.py
@@ -5,3 +5,9 @@ class ProductAdmin(admin.ModelAdmin):
     list_display=('product_name','price','stock','category','modified_date','is_available')
     prepopulated_fields={'slug':('product_name',)}
 admin.site.register(Product,ProductAdmin)
+
+class ProductVariationAdmin(admin.ModelAdmin):
+    list_display=('product','variation_category','variation_value','created','is_active')
+    list_editable=('is_active',)
+    list_filter=('product','variation_category','is_active')
+admin.site.register(ProductVariation,ProductVariationAdmin)
