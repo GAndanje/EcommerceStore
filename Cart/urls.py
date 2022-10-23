445c056e6cf4d53fadcc8c3f09a94788d113869d
Gilbert <gilbertandanje@gmail.com>
2022-10-23 11:44:43 +0300

    added cart_item_id argument in the url

--- Cart/urls.py
+++ Cart/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('',views.cart,name="cart"),
     path('add_cart/<int:product_id>/',views.add_cart,name="add_cart"),
-    path('decrement_cart/<int:product_id>/',views.decrement_cart,name="decrement_cart"),
-    path('remove_from_cart/<int:product_id>/',views.remove_from_cart,name="remove_from_cart"),
+    path('decrement_cart/<int:product_id>/<int:cart_item_id>/',views.decrement_cart,name="decrement_cart"),
+    path('remove_from_cart/<int:product_id>/<int:cart_item_id>/',views.remove_from_cart,name="remove_from_cart"),
 
 ]
