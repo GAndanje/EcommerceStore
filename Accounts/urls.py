dd3b97fe2c2e69011f6995bd7036942b4f62a2d8
Gilbert <gilbertandanje@gmail.com>
2022-10-27 00:21:20 +0300

    created login,register and logout views

---
+++ Accounts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('Register/',views.register,name="register"),
+    path('Login/',views.login,name='login'),
+    path('Logout/',views.logout,name='logout'),
+
+]
