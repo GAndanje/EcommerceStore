f0251b5160d1c161df8e62dbe9ef8bcb3b381df9
Gilbert <gilbertandanje@gmail.com>
2023-01-08 16:21:51 +0300

    this will import tools for account authentication over http

--- Accounts/views.py
+++ Accounts/views.py
@@ -1,9 +1,16 @@
+from django.core.mail import EmailMessage
 from django.contrib import messages,auth
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render,redirect
 from .forms import RegisterForm
 from .models import Account
+
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_decode,urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
 # Create your views here.
 def register(request):
     if request.method=='POST':
