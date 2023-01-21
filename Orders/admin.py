81398b10be23caab6b88280d726773ec32c4f3d1
Gilbert <gilbertandanje@gmail.com>
2023-01-21 10:16:12 +0300

    this commit will render the admin-side Order model for search, filter and display select fields

--- Orders/admin.py
+++ Orders/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Order,OrderProduct,Payment
 # Register your models here.
-admin.site.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display=['user','full_names','status','order_number','phone_number','email','country'  ,'city','order_total','tax','is_ordered','created_at','ip']
+    list_filter=['is_ordered','status','country']
+    list_per_page=20
+    search_fields=['first_name','last_name','phone_number','email']
+admin.site.register(Order,OrderAdmin)
 admin.site.register(Payment)
 admin.site.register(OrderProduct)
